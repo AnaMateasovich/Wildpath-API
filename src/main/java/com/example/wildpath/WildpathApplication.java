abe1ab708b45aecf6e264c7c2b95753ec77e0fdb
AnaMateasovich <anamateasovich98@gmail.com>
2025-07-30 15:15:21 -0300

    chore(config): add dotenv support and use env variables in application.properties

--- src/main/java/com/example/wildpath/WildpathApplication.java
+++ src/main/java/com/example/wildpath/WildpathApplication.java
@@ -1,5 +1,6 @@
 package com.example.wildpath;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -11,6 +12,13 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 public class WildpathApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+
 		SpringApplication.run(WildpathApplication.class, args);
 	}
 
